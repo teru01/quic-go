63f0052bcd2498f293efb24fa8f7b1d1ebf78389
Marten Seemann <martenseemann@gmail.com>
2019-11-27 19:22:48 +0700

    enforce the active_connection_id_limit

--- conn_id_manager.go
+++ conn_id_manager.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	mrand "math/rand"
 
+	"github.com/lucas-clemente/quic-go/internal/qerr"
+
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
@@ -57,7 +59,7 @@ func (h *connIDManager) Add(f *wire.NewConnectionIDFrame) error {
 		return err
 	}
 	if h.queue.Len() >= protocol.MaxActiveConnectionIDs {
-		h.updateConnectionID()
+		return qerr.ConnectionIDLimitError
 	}
 	return nil
 }
