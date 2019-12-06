674b94508404303fba1afbfe43fdacbbc260d920
teruya <teru0x01.sheep@gmail.com>
2019-12-06 13:54:00 +0900

    client for debug

--- example/client/main.go
+++ example/client/main.go
@@ -7,7 +7,6 @@ import (
 	"flag"
 	"io"
 	"net/http"
-	"sync"
 
 	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/lucas-clemente/quic-go/internal/testdata"
@@ -45,31 +44,23 @@ func main() {
 	hclient := &http.Client{
 		Transport: roundTripper,
 	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(urls))
-	for _, addr := range urls {
-		logger.Infof("GET %s", addr)
-		go func(addr string) {
-			rsp, err := hclient.Get(addr)
-			if err != nil {
-				panic(err)
-			}
-			logger.Infof("Got response for %s: %#v", addr, rsp)
-
-			body := &bytes.Buffer{}
-			_, err = io.Copy(body, rsp.Body)
-			if err != nil {
-				panic(err)
-			}
-			if *quiet {
-				logger.Infof("Request Body: %d bytes", body.Len())
-			} else {
-				logger.Infof("Request Body:")
-				logger.Infof("%s", body.Bytes())
-			}
-			wg.Done()
-		}(addr)
+	_ = urls
+	url := "https://localhost:6121"
+	rsp, err := hclient.Get(url)
+	if err != nil {
+		panic(err)
 	}
-	wg.Wait()
+	logger.Infof("Got response for %s: %#v", url, rsp)
+
+	body := &bytes.Buffer{}
+	_, err = io.Copy(body, rsp.Body)
+	if err != nil {
+		panic(err)
+	}
+	if *quiet {
+		logger.Infof("Request Body: %d bytes", body.Len())
+	} else {
+		logger.Infof("Request Body:")
+		logger.Infof("%s", body.Bytes())
+	}
 }
