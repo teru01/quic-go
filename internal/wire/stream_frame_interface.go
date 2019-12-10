ead7c7d374e5513c8ad4fe264f487cacd6e61d1f
teruya <teru0x01.sheep@gmail.com>
2019-12-10 17:48:03 +0900

    define interface

--- internal/wire/stream_frame_interface.go
+++ internal/wire/stream_frame_interface.go
@@ -1,6 +1,14 @@
 package wire
 
+import "github.com/lucas-clemente/quic-go/internal/protocol"
+
 type StreamFrameInterface interface {
 	Frame
+	GetDataLen() protocol.ByteCount
+	GetStreamId() protocol.StreamID
+	GetOffset() protocol.ByteCount
+	GetFinBit() bool
+	GetData() []byte
+	PutBack()
 	// Write(b *bytes.Buffer, version protocol.VersionNumber) error
 }
